a4b062e3af9656cbe63060faca7b704cf0487dc8
Alexander Wong <admin@alexander-wong.com>
2016-01-30 15:14:44 -0700

    Added map to index

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,12 +4,22 @@
 
 class MainController {
 
-  constructor($http) {
+  constructor($http, uiGmapGoogleMapApi) {
     this.$http = $http;
     this.awesomeThings = [];
+    this.map = {};
+    this.options = {};
+    var self = this;
 
     $http.get('/api/things').then(response => {
       this.awesomeThings = response.data;
+    });
+
+    uiGmapGoogleMapApi.then(function (maps) {
+      console.log("Map instantiated");
+      self.map = {center: {latitude: 51.219053, longitude: 4.404418}, zoom: 14};
+      self.options = {scrollwheel: false};
+      console.log(maps);
     });
   }
 
